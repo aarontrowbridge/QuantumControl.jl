bad29cf230ee09520b5dd13d06aa6ea1f20cf744
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2022-05-25 22:53:42 -0400

    cleaned up isodynamics refactor (still want jacobian!)

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -34,29 +34,9 @@ RD.@autodiff struct MultiQubitSystem <: RD.ContinuousDynamics
         nqubits = Int(log2(size(H_drift)[1]))
         isodim = 2^nqubits * 2
         nstates = nqstates * isodim
-        if isodynamics
-            H_drift_real = real(H_drift)
-            H_drift_imag = imag(H_drift)
-            H_drift_iso = SMatrix{isodim, isodim}(
-                [H_drift_real -H_drift_imag;
-                 H_drift_imag  H_drift_real]
-            )
-
-            H_drive_real = real(H_drive)
-            H_drive_imag = imag(H_drive)
-            H_drive_iso = SMatrix{isodim, isodim}(
-                [H_drive_real -H_drive_imag;
-                 H_drive_imag  H_drive_real]
-            )
-
-            return new{typeof(H_drift_iso)}(
-                nqubits, nstates, nqstates, isodim, isodynamics, H_drift_iso, H_drive_iso
-            )
-        else
-            return new{typeof(H_drift)}(
-                nqubits, nstates, nqstates, isodim, isodynamics, H_drift, H_drive
-            )
-        end
+        return new{typeof(H_drift)}(
+            nqubits, nstates, nqstates, isodim, isodynamics, H_drift, H_drive
+        )
     end
 end
 
@@ -66,62 +46,65 @@ RD.state_dim(model::MultiQubitSystem) = model.nstates + 3
 # single control dimension
 RD.control_dim(::MultiQubitSystem) = 1
 
-# function RD.dynamics!(model::MultiQubitSystem, ẋ, x, u)
-#     nstates = model.nstates
-
-#     ctrl_inds = (nstates + 1):(nstates + 3)
-#     ∫a, a, da = x[ctrl_inds]
-#     dda = u[1]
-#     xdot[ctrl_inds] .= [a, da, dda]
-
-#     H = model.H_drift + a * model.H_drive
-
-#     for i = 1:model.nqstates
-#         ψ = getqstate(x, i, model.isodim)
-
-#         ψ̇ = -im * H * ψ
-
-#         setqstate!(ẋ, ψ̇, i, model.isodim)
-#     end
-#     return nothing
-# end
-
-function RD.dynamics(model::MultiQubitSystem, x, u)
+function RD.dynamics!(model::MultiQubitSystem, ẋ, x, u)
     nstates = model.nstates
-
-    ẋ = similar(x)
 
     ctrl_inds = (nstates + 1):(nstates + 3)
     ∫a, a, da = x[ctrl_inds]
     dda = u[1]
-    ẋ[ctrl_inds] .= [a, da, dda]
-    # ẋ[nstates + 1] = a
-    # ẋ[nstates + 2] = da
-    # ẋ[nstates + 3] = dda
+
+    xdot[ctrl_inds] .= [a, da, dda]
 
     H = model.H_drift + a * model.H_drive
 
-    schroedinger!(ẋ, x, H, model)
+    if model.isodynamics
+        isoschroedinger!(ẋ, x, H, model.nqstates, model.isodim)
+    else
+        schroedinger!(ẋ, x, H, model.nqstates, model.isodim)
+    end
+end
+
+function RD.dynamics(model::MultiQubitSystem, x, u)
+    ẋ = similar(x)
+
+    nstates = model.nstates
+
+    ctrl_inds = (nstates + 1):(nstates + 3)
+    ∫a, a, da = x[ctrl_inds]
+    dda = u[1]
+
+    ẋ[ctrl_inds] .= [a, da, dda]
+
+    H = model.H_drift + a * model.H_drive
+
+    if model.isodynamics
+        isoschroedinger!(ẋ, x, H, model.nqstates, model.isodim)
+    else
+        schroedinger!(ẋ, x, H, model.nqstates, model.isodim)
+    end
 
     return SVector{nstates+3}(ẋ)
 end
 
-const I2 = SMatrix{2,2}(I(2))
+const Id2 = SMatrix{2,2}(I(2))
 const im2 = SMatrix{2,2}([0 1; -1 0])
 
-function schroedinger!(ẋ, x, H, model)
+function isoschroedinger!(ẋ, x, H, nqstates, isodim)
     Hreal = real(H)
     Himag = imag(H)
-    for i = 1:model.nqstates
-        ψiso = x[(1 + (i - 1) * model.isodim):(i * model.isodim)]
-        ψ̇iso = (kron(I2, Himag) + kron(im2, Hreal)) * ψiso
-        ẋ[(1 + (i - 1) * model.isodim):(i * model.isodim)] .= ψ̇iso
+    for i = 1:nqstates
+        ψiso = x[(1 + (i - 1) * isodim):(i * isodim)]
+        ψ̇iso = (kron(Id2, Himag) + kron(im2, Hreal)) * ψiso
+        ẋ[(1 + (i - 1) * isodim):(i * isodim)] .= ψ̇iso
     end
-    # for i = 1:model.nqstates
-    #     ψ = getqstate(x, i, model.isodim)
-    #     ψ̇ = -im * H * ψ
-    #     setqstate!(ẋ, ψ̇, i, model.isodim)
-    # end
+end
+
+function schroedinger!(ẋ, x, H, nqstates, isodim)
+    for i = 1:nqstates
+        ψ = getqstate(x, i, isodim)
+        ψ̇ = -im * H * ψ
+        setqstate!(ẋ, ψ̇, i, isodim)
+    end
 end
 
 function getqstate(x, i, isodim)
